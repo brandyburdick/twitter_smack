9fd2bb6ad5091032d8a1b6a02711c135795609a6
Brandy Burdick <blburdic@asu.edu>
2016-08-16 11:25:52 -0700

    Add epicenter controller, edit the feed and show_user pages of epicenter controller

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -1,5 +1,14 @@
 class EpicenterController < ApplicationController
   def feed
+  	# Instantiate empty array for holding all the tweets
+  	@following_tweets = []
+  	# Loop through all the tweets and find the relevant ones
+  	# TODO: Very inefficient query, look at better alternatives
+  	Tweet.all.each do |tweet|
+  		if current_user.following.include?(tweet.user_id)
+  			@following_tweets.push(tweet)                 
+  		end
+  	end
   end
 
   def show_user
