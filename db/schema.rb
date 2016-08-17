9a113823166020510aed5e02b46798a9b98a5be2
Brandy Burdick <blburdic@asu.edu>
2016-08-17 10:06:54 -0700

    Create tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816175344) do
+ActiveRecord::Schema.define(version: 20160817170432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "phrase"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string   "message"
