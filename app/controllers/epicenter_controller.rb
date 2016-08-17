f07eeee8031f99b974124009cd2a74529f4096c8
Brandy Burdick <blburdic@asu.edu>
2016-08-17 09:26:34 -0700

    Add unfollow functionality

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -4,19 +4,29 @@ class EpicenterController < ApplicationController
   	@following_tweets = []
   	# Loop through all the tweets and find the relevant ones
   	# TODO: Very inefficient query, look at better alternatives
-  	Tweet.all.each do |tweet|
-  		if current_user.following.include?(tweet.user_id)
-  			@following_tweets.push(tweet)                 
-  		end
-  	end
+  	if user_signed_in?
+      Tweet.all.each do |tweet|
+    		if current_user.following.include?(tweet.user_id)
+    			@following_tweets.push(tweet)                 
+    		end
+    	end
+    end
   end
 
   def show_user
+  	@user = User.find(params[:id])
   end
 
   def now_following
+    @user = User.find(params[:id])
+
+    current_user.following.push(params[:id].to_i)
+    current_user.save
   end
 
   def unfollow
+    @user = User.find(params[:unfollow_id])
+    current_user.following.delete(params[:unfollow_id].to_i)
+    current_user.save
   end
 end
